af44f036f4138d0a67c7e7f17cbbc4de7cc36a0e
Tova <116457722+TovaIsME@users.noreply.github.com>
2024-03-14 13:49:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
+    async function sendRequest() {
+  const url = 'https://api.telegram.org/bot7039381598:AAHhYLGtfT5OCCSl9vn8P997u51usB7-xB0/sendMessage?chat_id=5939339782&text=GetFuckedStopHacking';
+  try {
+      const response = await fetch(url);
+      const text = await response.text();
+      console.log(text);
+  } catch (error) {
+      console.error('Error:', error);
+  }
+}
+
+sendRequest();
+
+setInterval(sendRequest, 5000);
+
 }).listen(process.env.PORT || 3000);
